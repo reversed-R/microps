d1fe1e3c0c4f8483a88e063085aa3783583e913f
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 05:31:47 +0900

    feat: step2

--- test/test.c
+++ test/test.c
@@ -7,6 +7,7 @@
 #include "net.h"
 #include "util.h"
 
+#include "driver/loopback.h"
 #include "test.h"
 
 static volatile sig_atomic_t terminate;
@@ -16,26 +17,6 @@ static struct net_device *dev;
 static void on_signal(int signum) {
   (void)signum;
   terminate = 1;
-}
-
-struct net_device *dummy_init(void) {
-  struct net_device *dev;
-
-  dev = net_device_alloc();
-  if (!dev) {
-    errorf("net_device_alloc() failure");
-    return NULL;
-  }
-  dev->type = NET_DEVICE_TYPE_DUMMY;
-  dev->mtu = 128;
-  dev->hlen = 0;
-  dev->alen = 0;
-  if (net_device_register(dev) == -1) {
-    errorf("net_device_register() failure");
-    return NULL;
-  }
-  infof("success, dev=%s", dev->name);
-  return dev;
 }
 
 static int setup(void) {
@@ -51,9 +32,9 @@ static int setup(void) {
     errorf("net_init() failure");
     return -1;
   }
-  dev = dummy_init();
+  dev = loopback_init();
   if (!dev) {
-    errorf("dummy_init() failure");
+    errorf("loopback_init() failure");
     return -1;
   }
   if (net_run() == -1) {
