f5aea05785d41234dd0008015c2f7d3ff2b943cd
reversed-R <qiu1287shan@gmail.com>
2026-07-20 19:38:50 +0900

    feat: step11

--- platform/linux/platform.c
+++ platform/linux/platform.c
@@ -1,76 +1,56 @@
-#include <stdint.h>
-#include <stddef.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <time.h>
 
 #include "platform.h"
 
+#include "intr.h"
 #include "util.h"
 
-int
-platform_init(void)
-{
-    srandom(time(NULL));
-    return 0;
+int platform_init(void) {
+  srandom(time(NULL));
+  if (intr_init() == -1) {
+    return -1;
+  }
+  return 0;
 }
 
-int
-platform_run(void)
-{
-    return 0;
+int platform_run(void) {
+  if (intr_run() == -1) {
+    return -1;
+  }
+  return 0;
 }
 
-int
-platform_shutdown(void)
-{
-    return 0;
+int platform_shutdown(void) {
+  if (intr_shutdown() == -1) {
+    return -1;
+  }
+  return 0;
 }
 
 /*
  * Memory
  */
 
-void *
-memory_alloc(size_t size)
-{
-    return calloc(1, size);
-}
+void *memory_alloc(size_t size) { return calloc(1, size); }
 
-void
-memory_free(void *ptr)
-{
-    free(ptr);
-}
+void memory_free(void *ptr) { free(ptr); }
 
 /*
  * Lock
  */
 
-int
-lock_init(lock_t *lock)
-{
-    return pthread_mutex_init(lock, NULL);
-}
+int lock_init(lock_t *lock) { return pthread_mutex_init(lock, NULL); }
 
-int
-lock_acquire(lock_t *lock)
-{
-    return pthread_mutex_lock(lock);
-}
+int lock_acquire(lock_t *lock) { return pthread_mutex_lock(lock); }
 
-int
-lock_release(lock_t *lock)
-{
-    return pthread_mutex_unlock(lock);
-}
+int lock_release(lock_t *lock) { return pthread_mutex_unlock(lock); }
 
 /*
  * Random
  */
 
-uint16_t
-random16(void)
-{
-    return random() % (UINT16_MAX+1);
-}
+uint16_t random16(void) { return random() % (UINT16_MAX + 1); }
--- test/test.c
+++ test/test.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "driver/ether_tap.h"
 #include "icmp.h"
 #include "ip.h"
 #include "net.h"
@@ -49,6 +50,20 @@ static int setup(void) {
     errorf("ip_iface_register() failure");
     return -1;
   }
+  dev = ether_tap_init(ETHER_TAP_NAME, ETHER_TAP_HW_ADDR);
+  if (!dev) {
+    errorf("ether_tap_init() failure");
+    return -1;
+  }
+  iface = ip_iface_alloc(ETHER_TAP_IP_ADDR, ETHER_TAP_NETMASK);
+  if (!iface) {
+    errorf("ip_iface_alloc() failure");
+    return -1;
+  }
+  if (ip_iface_register(dev, iface) == -1) {
+    errorf("ip_iface_register() failure");
+    return -1;
+  }
   if (net_run() == -1) {
     errorf("net_run() failure");
     return -1;
@@ -66,22 +81,8 @@ static int cleanup(void) {
 }
 
 static int app_main(void) {
-  ip_addr_t src, dst;
-  uint16_t id, seq = 0;
-  uint32_t val;
-  uint8_t data[] = {'T', 'E', 'S', 'T'};
-
-  ip_addr_pton(LOOPBACK_IP_ADDR, &src);
-  dst = src;
-  id = getpid() % UINT16_MAX;
   debugf("press Ctrl+C to terminate");
   while (!terminate) {
-    val = hton32(id << 16 | ++seq);
-    if (icmp_output(ICMP_TYPE_ECHO, 0, val, data, sizeof(data), src, dst) ==
-        -1) {
-      errorf("icmp_output() failure");
-      break;
-    }
     sleep(1);
   }
   debugf("terminate");
